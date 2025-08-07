a83ddc304175a1f0ac2f14a79c38a85033ee96a4
rchubak <romanchubak@gmail.com>
2025-08-07 23:02:29 +0200

    add github copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,24 @@
+return {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "InsertEnter",
+    config = function()
+        require("copilot").setup({
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                debounce = 75,
+                keymap = {
+                    accept = "<C-a>", -- Ctrl + Enter
+                },
+            },
+            panel = { enabled = false },
+            filetypes = {
+                ["*"] = true,
+            },
+            copilot_node_command = "node", -- adapt if needed
+        })
+        vim.g.copilot_assume_mapped = true
+    end,
+}
